16c004a879f2180e3aca872a7f98b4897bef8bc6
Nan devv <nandafitrohardiansyah2812@gmail.com>
2026-06-04 09:35:11 +0700

    Fix GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,20 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Crypto-App/',
+
   plugins: [
     vue(),
     legacy()
   ],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+
   test: {
     globals: true,
     environment: 'jsdom'
